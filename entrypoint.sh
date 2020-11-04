7ddab592087f64bb3d9c973ea79bb264c90727d6
ValletRomain <romainvallet@live.fr>
2020-11-04 18:50:53 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ mkdir --parent $OUT_DIR
 for fileName in *.tex; do
     [ -f "$fileName" ] || break
 
-    echo "Substituting version number ${GITHUB_SHA::7} in file $fileName..."
-    sed -i -e "s/verSubstitution/${GITHUB_SHA::7}/" $fileName
+    #echo "Substituting version number ${GITHUB_SHA::7} in file $fileName..."
+    #sed -i -e "s/verSubstitution/${GITHUB_SHA::7}/" $fileName
 
     echo "Converting file $fileName to pdf..."
     xelatex $fileName
